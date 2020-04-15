241523d85150f642a2d7cb2a99ed3235c9242c6a
Milos Smiljanic <simple.smiki@gmail.com>
2020-04-15 19:02:39 +0200

    sva logika u js fajlu, provera lokacije dodata

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ url - https://mogunapolje.github.io/sada/
 
 Za uraditi:
 - [x] v0.0.1 stranica koja kaze negativno (vreme do kog ce biti ispravna - 13.04.2020. 05:00)
-- [ ] v0.0.2 dodati logiku koja prati aktuelne restrikcije 
-- [ ] v0.0.3 dodati proveru lokacije
+- [x] v0.0.2 dodati proveru lokacije, dodati logiku koja prati aktuelne restrikcije 
+- [ ] v0.0.3 dodati logiku koja prati vikend restrikcije
 - [ ] v0.0.4 dodati vremensku prognozu
 
 Lepo za imati: 
--- javascript.js
+++ javascript.js
@@ -1,9 +1,27 @@
-$.get("https://ipinfo.io", function (response) {
-    // $("#ip").html("IP: " + response.ip);
-    console.log(response.city, response.country);
+$(document).ready(function() {
 
-// todo pokazi lepo srpsku neosisanu latinicu
-    $("#grad").html("Nalazis se u  " + response.city + ", " + response.region);
-    // $("#zemlja").html("Kod zemlje: " + response.country);
-    // $("#details").html(JSON.stringify(response, null, 4));
-}, "jsonp");
+	$.get("https://ipinfo.io", function (response) {
+	    // $("#ip").html("IP: " + response.ip);
+	    console.log(response.city, response.country);
+
+		// todo pokazi lepo srpsku neosisanu latinicu
+	    $("#grad").html("Nalazis se u  " + response.city + ", " + response.region);
+
+	    if (response.country !== "RS") {
+	    	$("#odgovor").html("Mozes tebra, ti ne zivis u Srbijici majci!");
+	    } else {
+	    	$("#odgovor_vpn").hide();
+	    	var d = new Date();
+  			var h = d.getHours();
+  			if (h >= 5 && h < 17) {
+    			$("#odgovor").html("Imaš fore do 5 popodne. Uhvati dan!");
+  			} else {
+  				$("#odgovor").html("Nikako buraz, zakasnio si.");
+  			}
+	    }
+	    // $("#zemlja").html("Kod zemlje: " + response.country);
+	    // $("#details").html(JSON.stringify(response, null, 4));
+	}, "jsonp");
+
+
+});
