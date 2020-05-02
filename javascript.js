21e68618c6a21f1d9a516f62f63816ec53618836
Milos Smiljanic <simple.smiki@gmail.com>
2020-05-02 12:55:18 +0200

    dodata vremenska prognoza

--- javascript.js
+++ javascript.js
@@ -34,6 +34,9 @@ $(document).ready(function() {
 	    // $("#details").html(JSON.stringify(response, null, 4));
 	}, "jsonp");
 
+	
+	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
 
 });
 
